1dbb97fd8180d5df73928ed54132ab39bc32a28b
Angel1485 <andyjp_666@hotmail.com>
2026-04-22 17:48:59 -0500

    ejercicios for

---
+++ ejercicios_for/ejercicio.js
@@ -0,0 +1,43 @@
+function listarNumeros()
+{
+    for (let i=0 ; i<=3; i++)
+    {
+        console.log(i); 
+    }
+}
+
+function ejecutar(numEjercicio)
+{
+    if (numEjercicio === 1)
+    {
+        listarNumeros(); 
+    }
+    if (numEjercicio === 2)
+    {
+        listarNumerosReversa(); 
+    }
+    if (numEjercicio === 3)
+    {
+        listarPares(); 
+    }
+     if (numEjercicio === 4)
+    {
+        listarImpares(); 
+    }
+}
+
+function listarNumerosReversa()
+{
+    for (let i=3 ; i>0; i--)
+    {
+        console.log(i);
+    }
+}
+
+function listarPares()
+{
+    for (let i=0 ; i<10; i += 2)  /// operador abreviado
+    {
+        console.log(i);
+    }
+}
